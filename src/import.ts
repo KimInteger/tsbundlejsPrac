590de8ddbb3c80e1ac6a7ab85b4128bfacf2a7b0
KimInteger <kjs20151240@gmail.com>
2024-07-04 14:35:45 +0900

    feat_김정수 : 함수생성 및 변수 할당

--- src/import.ts
+++ src/import.ts
@@ -4,3 +4,10 @@ import muliply from './muliply';
 console.log(add(1, 3));
 
 console.log(muliply(3, 4));
+
+function addMuliply(a: number, b: number): void {
+  const plus = a + b;
+  console.log(plus);
+}
+
+addMuliply(add(5, 6), muliply(3, 4));

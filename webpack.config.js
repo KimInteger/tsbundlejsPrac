662cfd549eee3cef4fd42a9b2c2b0853d7f31533
KimInteger <kjs20151240@gmail.com>
2024-07-04 14:42:18 +0900

    chore_김정수 : bundling Error

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry : "./dist/menu.js",
-  output : {
-    path : path.resolve(__dirname,'dist'),
-    filename : 'bundle.js'
+  entry: './src/import.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
         test: /\.ts$/,
-        use : 'ts-loader',
-        exclude : /node_modules/
-      }
-    ]
-  }
-}
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
